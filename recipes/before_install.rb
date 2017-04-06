b56f7f4d47518242ca7d2cc20e63fdd36a85d7b0
Timofey Kirillov <timofey.kirillov@flant.com>
2017-04-06 11:59:55 +0300

    ++

--- recipes/before_install.rb
+++ recipes/before_install.rb
@@ -2,7 +2,7 @@ include_recipe 'apt'
 
 node.default['rvm']['gpg'] = {}
 node.default['rvm']['install_rubies'] = true
-node.default['rvm']['rubies'] = ['2.3.1']
-node.default['rvm']['default_ruby'] = node['rvm']['rubies'].first
+node.default['rvm']['rubies'] = [node['dimod-example-ruby']['ruby_version']]
+node.default['rvm']['default_ruby'] = node['dimod-example-ruby']['ruby_version']
 node.default['rvm']['global_gems'] = [{name: 'bundler'}]
 include_recipe 'rvm::system'
